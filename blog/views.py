1b775ab25c2b5673adf4a6eff3711619cddd3858
Jigeeshu <jigeeshu23@gmail.com>
2017-04-24 11:08:02 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
